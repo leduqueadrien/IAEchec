6b39549866482cc731dbc58b8ce01caefc3c7235
adrien <aleduque@ensc.fr>
2020-03-14 18:28:31 +0100

    recuperation des fichiers

--- RecuperationData/RecuperationFichier.c
+++ RecuperationData/RecuperationFichier.c
@@ -5,50 +5,54 @@
 // commande a executer : 
 // wget https://chesstempo.com/requests/download_game_pgn.php?gameids= + ID
 
+//commande a executer dans le terminal : nohup ./prog > sortieStandard
+
+#define TAILLEID 8
+
 char * CreationCommande (char * URL, char * ID);
 
 int main() {
     
     char fileName [25] = "IDPartieTest.txt";
-    char URL [100] = "https://chesstempo.com/requests/download_game_pgn.php?gameids=";
+    char URL [100] = "https://old.chesstempo.com/requests/download_game_pgn.php?gameids=";
     
-    int tailleID = 7;
-    FILE * file = NULL;
-    file = fopen(fileName, "r");
+    FILE * file = fopen(fileName, "r");
 
     if (file == NULL) {
         printf("Le fichier \"%s\" n'a pu être ouvert\n", fileName);
     } else {
         printf("Le fichier \"%s\" est ouvert\n", fileName);
 
-        printf("Création du répertoir\n");
+        printf("Création du répertoire\n");
 
-        if (system("mkdir dataBasChess") == 1) {
-            printf("Le répertoir n'a pas pu être créé\n");
+        if (system("mkdir dataBaseChess") == 1) {
+            printf("Le répertoire n'a pas pu être créé\n");
         } else {
-            printf("Le répertoir a été créé\n");
+            printf("Le répertoire a été créé\n\n");
 
-            char * strID = malloc(sizeof(char)*tailleID);
+            char strID [TAILLEID];
             char * commandecomplete;
+            int cpt = 0;
             while (feof(file) == 0) {
-                int i;
-                for (i=0; i<tailleID; i++) {
-                    fscanf(file, "%c", (strID+i));  // A changer. fgets ?
-                }
-                commandecomplete = CreationCommande(URL, strID);
-                printf("%s\n", commandecomplete);
-                printf("Telechargement du fichier \"%s\"\n", strID);
+                cpt++;
+                fgets(strID, TAILLEID, file);
 
+                commandecomplete = CreationCommande(URL, strID);
+                //printf("Telechargement du fichier \"%s\"\n", strID);
+                //printf("%s\n", commandecomplete);
+                
                 system( commandecomplete );
-                printf("Telechargement terminé\n");
+                //printf("Telechargement terminé\n\n");
                                 
                 fscanf(file, "%c", strID);
-                
+
+                if (cpt%10 == 0)
+                    printf("%d parties\n", cpt);
+
             }
-            free(strID);
             free(commandecomplete);
+            printf("\nFIN\n%d parties telechargées\n", cpt);
         }
-        
         fclose(file);
     }
         
@@ -59,7 +63,7 @@ int main() {
 
 char * CreationCommande (char * URL, char * ID){
     int tailleURL = strlen(URL);
-    char commande [] =  "wget ./dataBasChess ";
+    char commande [] =  "wget -P ./dataBaseChess ";
     int taillecommande = strlen(commande);
     char * CommandeComplete = malloc(sizeof(char)*(taillecommande + tailleURL + 7));
     int i;
